e154388d13ba87b0d3c5fba61c40172f6a38d1d4
samwhindleton <samwhindleton@gmail.com>
2018-03-02 21:49:46 -0700

    declared variables for containers, created start button click event listener

--- js/listeners.js
+++ js/listeners.js
@@ -1,11 +1,49 @@
 // console.log("jQuery loaded: " + $);
 
+// ########################################
+// # variables
+// ########################################
+//
+// containers
+// assign $charContainer to value #char-container
+const $charContainer = $("#char-container");
+// assign $gameContainer to value #game-container
+const $gameContainer = $("#game-container");
+// assign $titleContainer to value #title-container
+const $titleContainer = $("#title-container");
+// assign $socialMediaContainer to value #social-media-container
+const $socialMediaContainer = $("#social-media-container");
+// assign $startButton to value #start-button>h1
+const $startButton = $("#start-button>h1");
+//
+// game charaters
 // assign $tribble to value .tribble
 const $tribbles = $(".tribbles");
-// assign $tribble to value .klingons
+// assign $klingons to value .klingons
 const $klingons = $(".klingons");
 
-// click event listeners
+// ########################################
+// # click event listeners
+// ########################################
+//
+// listen for $startButton click
+$($startButton).click((event) => {
+  // console log if clicked
+  console.log("I clicked START");
+  console.log(points);
+  // detach $startButton, $socialMediaContainer, $titleContainer
+  $startButton.detach();
+  $socialMediaContainer.detach();
+  $titleContainer.detach();
+  // change height of $gameContainer and $charContainer
+  $gameContainer.css({
+    height:"100vh",
+  });
+  $charContainer.css({
+    height:"100vh",
+  });;
+// end $startButton click event listener function
+});
 // listen for $tribble click
 $($tribbles).click((event) => {
   // console log if clicked
@@ -14,7 +52,7 @@ $($tribbles).click((event) => {
   console.log(points);
 // end $tribble click event listener function
 });
-
+//
 // listen for $klingons click
 $($klingons).click((event) => {
   // console log if clicked
